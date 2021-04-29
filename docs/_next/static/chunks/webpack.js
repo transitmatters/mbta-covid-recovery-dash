1c6600ceb9a936ea80ae80735b7216ec6bf68e28
Ian Reynolds <idreyn@gmail.com>
2021-04-29 12:06:55 -0400

    add logo to header

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a4794fae502e48cdf656";
+/******/ 	var hotCurrentHash = "2fcc70140d3da7c34872";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
