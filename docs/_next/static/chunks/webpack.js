461a226e03c58959c3be19e9c5d023f9132ec4ca
Ian Reynolds <idreyn@gmail.com>
2021-11-07 20:54:53 -0500

    Rebuild with mobile TopLine
    okay, actually commit new build

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e39ee2536f89b877a12e";
+/******/ 	var hotCurrentHash = "fcaf68e5ecbb6cc96b82";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
