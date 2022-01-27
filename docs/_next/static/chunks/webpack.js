75710ccc21261cf0b22a4482fd4986238c0cdbc2
Ian Reynolds <idreyn@gmail.com>
2022-01-27 15:54:09 -0500

    Data update: 21427(date -R)

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "43e35d2185aa6a2ce27a";
+/******/ 	var hotCurrentHash = "358d1aed1b909464a8c8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
