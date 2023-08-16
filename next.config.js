d2792bcc4aa9202db271bc33c0aa5ffe5e9c4eba
Devin Matte <devinmatte@gmail.com>
2023-08-15 21:01:45 -0400

    Updating packages reasonably for Aug 2023 (#54)

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const hackStylesToSupportNonPureDeclarations = (config) => {
     const oneOf = config.module.rules.find((rule) => typeof rule.oneOf === "object");
 
     const fixUse = (use) => {
-        if (use.loader.indexOf("css-loader") >= 0 && use.options.modules) {
+        if (use.loader && use.loader.indexOf("css-loader") >= 0 && use.options.modules) {
             use.options.modules.mode = "local";
         }
     };
@@ -21,17 +21,14 @@ const hackStylesToSupportNonPureDeclarations = (config) => {
     return config;
 };
 
-const assetPrefix = process.env.BUILD_GH_PAGES === "true" ? "./" : null;
+const assetPrefix = process.env.BUILD_GH_PAGES === "true" ? "./" : undefined;
 
 module.exports = {
     webpack: (config) => {
         return hackStylesToSupportNonPureDeclarations(config);
     },
-    // https://stackoverflow.com/questions/65487914/error-image-optimization-using-next-js-default-loader-is-not-compatible-with-n
-    // On Next.js 12.3 we can use unoptimized: true instead
     images: {
-        loader: "akamai",
-        path: "",
+        unoptimized: true
     },
     assetPrefix,
 };
